e3c69049602c799d316b765dc4e0f9c04bd4cd5e
Jhoss <jhosser@live.com>
2024-06-12 23:18:47 -0500

    add google analytics, favicon and name

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,9 +3,11 @@ import lock from "./svg/lock.fill.svg";
 import unlock from "./svg/lock.open.fill.svg";
 import dollarfill from "./svg/dollar.fill.svg"
 import download from "./svg/icloud.and.arrow.down.svg";
+import favicoma from "./svg/favicoma.svg";
 
 export const herologo = heroimg;
 export const lockicon = lock;
 export const unlockicon = unlock;
 export const dollar = dollarfill;
 export const saveprint = download;
+export const favicon = favicoma;
